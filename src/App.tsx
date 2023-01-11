b5e8cca75545b1d935f104ef5382ba8b69936f34
Herlander Bento <herlanderbento19@gmail.com>
2023-01-11 20:40:52 +0100

    sections:correcting import

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ import { defaultTheme } from './styles/themes/default'
 
 import Header from './components/Header'
 import NavProvider from './contexts/NavContext'
-import Home from './sections/home'
 import News from './sections/News'
-import Projects from './sections/projects'
 import Services from './sections/services'
 import Team from './sections/Team'
 import Contacts from './sections/Contacts'
@@ -14,6 +12,8 @@ import AboutUS from './sections/about-us'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { ClientesAndCollaborators } from './sections/clients-and-collaborators'
+import Home from './sections/home'
+import Projects from './sections/projects'
 
 function App() {
   return (
